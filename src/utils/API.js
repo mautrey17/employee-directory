85de0c1f784c8c4ee9d4ef3665526ca603f848e2
Matt Autrey <mautrey17@gmail.com>
2021-04-21 18:47:25 -0400

    removed extra console log from API

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,7 +3,6 @@ import axios from "axios";
 export default {
     search: function(query) {
         let search = query ? `?${query}` : '?results=15';
-        console.log('https://randomuser.me/api/' + search);
         return axios.get('https://randomuser.me/api/' + search)
     }
 }
